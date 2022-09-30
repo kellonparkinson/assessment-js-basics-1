18883e7184968bc90cd0391bd3f2fb7f560befbe
Kellon Parkinson <kellon.digital@gmail.com>
2022-09-30 10:11:29 -0600

    completed sdlc portion

---
+++ sdlc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = 'In this second step, developers determine what their application is supposed to do and what requirements it will need to have in order to do those things.'
+
+const design = 'In this third step, developers create a model for their software. This includes the UI, the structure, the language they will use, and any other necessary elements in actually building a working model.'
+
+const implementation = 'The fourth step is where developers begin writing the actual code for their program so that the application can actually run. This is the building phase.'
+
+const testingAndIntegration = 'The fifth step is important for making sure the software is working properly, including finding/fixing bugs, and testing each part of the code to make sure it functions. Then the software can be deployed.'
+
+const maintain = 'Once the software has been deployed and is being used, more issues can be found by users. Developers need to maintain and fix the software during this phase so that users can have a properly functioning experience.'
