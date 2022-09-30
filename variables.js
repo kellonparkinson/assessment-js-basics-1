09519a4098c34394bc2cab08c55fd690d06788e5
Kellon Parkinson <kellon.digital@gmail.com>
2022-09-30 10:53:13 -0600

    adjusted spacing for readability

--- variables.js
+++ variables.js
@@ -2,18 +2,21 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+
 let myName = 'Kellon';
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+
 let favNum = '8';
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
 let lovesCode = true;
 
 
@@ -25,6 +28,7 @@ let lovesCode = true;
 */
 
 //CODE HERE
+
 if (favNum === 13) {
   console.log('lucky 13!')
 } else {
@@ -37,6 +41,7 @@ if (favNum === 13) {
 */
 
 //CODE HERE
+
 for (let i = 0; i < 5; i++) {
   console.log(`Favorite number: ${favNum}`)
 };
